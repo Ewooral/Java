d0c49c26f2f5c0ffedbfa8fcb03f46141a413c68
Ewooral <boahen.cos@gmail.com>
2022-12-29 09:00:26 +0000

    ...

--- basics/AnnotationDefinition.java
+++ basics/AnnotationDefinition.java
@@ -1,5 +1,7 @@
-/*  ANNOTATION TUTORIALS ON YOUTUBE  
- * @ https://youtu.be/DkZr7_c9ry8
+
+/**  
+ *   * ANNOTATION TUTORIALS ON YOUTUBE  
+ *   ? https://youtu.be/DkZr7_c9ry8
  */
 
 import java.lang.annotation.ElementType;
@@ -7,7 +9,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// annotation
+// ?  annotation
 @VeryImportant
 public class AnnotationDefinition {
     @ImportantString
@@ -20,7 +22,7 @@ public class AnnotationDefinition {
     private String address;
     private String schoolStatus;
 
-    // constructor
+    // ? constructor
     public AnnotationDefinition(String FIRSTNAME, String LASTNAME, int AGE,
             String ADDRESS, String SCHOOLSTATUS, String MAIDENNAME) {
         this.firstname = FIRSTNAME;
@@ -31,7 +33,7 @@ public class AnnotationDefinition {
         this.maidenName = MAIDENNAME;
     }
 
-    // annotation
+    // ? annotation
     @RunImmediately
     public void getLastName() {
         System.out.println("Lastname: " + this.lastname);
@@ -41,51 +43,56 @@ public class AnnotationDefinition {
         System.out.println("Firstname: " + this.firstname);
     }
 
-    // annotation
+    // ? annotation
     @RunImmediately(times = 3)
     public void getAge() {
         System.out.println(this.age);
     }
 
-    // annotation
+    // ? annotation
     @RunImmediately(times = 2)
     public void getSchoolStatus() {
         System.out.println("school status: " + this.schoolStatus);
     }
 
-    // annotation
+    // ? annotation
     @RunImmediately
     public void getAddress() {
         System.out.println("Address: " + this.address);
     }
 }
 
-// CREATE CUSTOM ANNOTATIONS
-/*
+// ! CREATE CUSTOM ANNOTATIONS
+/**
  * @Target annotation allows to specify which kind
- * of java element this annotation is valid to be used on
- * ElementType.TYPE is for class, .METHOD is for methods
+ *         of java element this annotation is valid to be used on
+ *         ElementType.TYPE is for class, .METHOD is for methods
  * 
  * @ Runtime Rentention tells java to keep this annotation around
  * through the running of the program so that other code can use it while
  * the program is running
  */
-@Target({ ElementType.TYPE, ElementType.METHOD }) // used on class
+
+// * annotation for both classes and methods
+@Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @interface VeryImportant {
 
 }
 
+// * annotation for only methods
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @interface RunImmediately {
-    // add parameter to annotation
+
+    // ! add parameter to annotation
     // they can only be a primitive type, class type, array type, String type
     // default value can be passed as parameters
     int times() default 1;
 
 }
 
+// * annotation for only fields or intance variables
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @interface ImportantString {
