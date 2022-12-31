00be620a75f624dc86552891e0cfd1023c0fca41
Ewooral <boahen.cos@gmail.com>
2022-12-31 06:57:20 +0000

    chore: stacks and queues in java

--- data_structures/Stacks.java
+++ data_structures/Stacks.java
@@ -1,42 +1,59 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Stacks {
-    public static ArrayList<Integer> customStack = new ArrayList<>();
-    public ArrayList<Integer> cStack = new ArrayList<>();
+    public static ArrayList<Integer> arr = new ArrayList<>();
 
-    public void buildStack(ArrayList<Integer> arr) {
-        for (int i = arr.size() - 1; i != -1; i--) {
-            int value = arr.get(i);
-            cStack.add(value);
-        }
-    }
-
-    public void push(int item) {
-        customStack.add(item);
-    }
-
-    public void printStack() {
-        buildStack(customStack);
-        System.out.println(cStack);
-    }
-
-    public int peek() {
-        return cStack.get(0);
-    }
-
-}
-
-class runCode {
+    // public void reverse(ArrayList<Integer> arr) {
+    // for (int i = arr.size() - 1; i != -1; i--) {
+    // int value = arr.get(i);
+    // cStack.add(value);
+    // }
+    // }
+    // [12, 2, -9, 0, 4, 1]
     public static void main(String[] args) {
-        Stacks newStack = new Stacks();
-        newStack.push(12);
-        newStack.push(2);
-        newStack.push(-9);
-        newStack.push(0);
-        newStack.push(4);
-        newStack.push(1);
-        newStack.printStack();
-        System.out.println(newStack.peek());
+        push(12);
+        push(2);
+        push(-9);
+        push(0);
+        push(4);
+        push(1);
+        buildStack(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
+        buildStack(arr);
 
     }
+
+    public static void buildStack(ArrayList<Integer> arr) {
+        int i = 0;
+        int j = arr.size() - 1;
+        int end = arr.get(i);
+        int begin = arr.get(j);
+        while (i < j) {
+            end = arr.get(j);
+            begin = arr.get(i);
+            arr.set(i, end);
+            arr.set(j, begin);
+            i++;
+            j--;
+        }
+        System.out.println(arr);
+    }
+
+    public static void push(int item) {
+        arr.add(item);
+    }
+
+    // public void printStack() {
+    // reverse(customStack);
+    // System.out.println(cStack);
+    // }
+
+    // public int peek() {
+    // return cStack.get(0);
+    // }
+
+    // public int pop() {
+    // return cStack.remove(0);
+    // }
+
 }
