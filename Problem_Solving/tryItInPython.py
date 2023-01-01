9599c7ef6e29541adc305f278842b1960dfd2735
Ewooral <boahen.cos@gmail.com>
2023-01-01 23:46:26 +0000

    chore: minimum value in an array without using java

--- Problem_Solving/tryItInPython.py
+++ Problem_Solving/tryItInPython.py
@@ -1,15 +1,35 @@
+from typing import List
+
+
 def swap(arr):
-    hash_mapOrDictionary = {};
     i = 0;
     j = len(arr) - 1;
     while i < j:
-        if arr[i] not in hash_mapOrDictionary:
-            hash_mapOrDictionary[i] = arr[i];
         arr[i], arr[j] = arr[j], arr[i];
         i+=1;
         j-=1;
-        hash_mapOrDictionary[i] = True;
-    print(hash_mapOrDictionary)
     return arr;
 
-print(swap([2, 4, 5, 1]));
+print(swap([2, 4, 1, 5, 0]));
+# 4, 2, 5, 1
+# 
+
+
+def min_value(arr: list):
+        i, j = 0, 1 
+        mini_value = arr[i]
+        while j < len(arr):
+            curV = arr[j]
+            if curV < mini_value:
+                mini_value = curV;
+            j += 1;
+        return min_value;
+
+print(min_value([2, 4, 1, 5, 0]))
+
+b = [4, 2]
+d = b[0]
+print(d)
+
+        
+
