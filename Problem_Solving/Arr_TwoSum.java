c0534e67b63dc0d8db9c45aaa15f7db9709ffeca
Ewooral <boahen.cos@gmail.com>
2023-01-06 23:46:51 +0000

    ...

--- Problem_Solving/Arr_TwoSum.java
+++ Problem_Solving/Arr_TwoSum.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Arr_TwoSum {
 
@@ -7,7 +9,22 @@ public class Arr_TwoSum {
     // find two numbers when added sum up to target.
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(2, 4, -3, 1, 3, 0, -3, -2));
+        ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(-1, -2, -3, -4, -5));
         twoSum(arr, 1);
+        System.out.println("................");
+        twoSum(arr1, -8);
+        System.out.println("................");
+        Solution s = new Solution();
+        int[] ar = { -1, -2, -3, -4, -5 };
+        int[] ans = s.twoSum(ar, -8);
+        int[] ans1 = two_sum(ar, -8);
+        for (int num : ans) {
+            System.out.println(num);
+        }
+        System.out.println("................");
+        for (int num : ans1) {
+            System.out.println(num);
+        }
 
     }
 
@@ -24,5 +41,75 @@ public class Arr_TwoSum {
             }
         }
 
+    }
+
+    static int[] two_sum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
+
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int curElem = nums[i];
+            int lastElem = nums[j];
+            if (curElem + lastElem > target) {
+                j--;
+            }
+            if (curElem + lastElem < target) {
+                i++;
+            }
+            if (i == j && curElem + lastElem < target) {
+                j++;
+            } else {
+                if (curElem + lastElem == target) {
+                    int[] idx = { i, j };
+                    return idx;
+                }
+
+            }
+        }
+        int[] emptyArr = {};
+        return emptyArr;
+    }
+
+    public int[] two_Sum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+
+    public int[] TwoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+        return null;
     }
 }
---
+++ data_structures/Implement_HashMap.java
@@ -0,0 +1,6 @@
+
+public class Implement_HashMap {
+    public static void main(String[] args) {
+
+    }
+}
