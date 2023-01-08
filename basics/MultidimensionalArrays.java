c926e9489048868ba97c4796be842ae435496987
Ewooral <boahen.cos@gmail.com>
2023-01-08 23:26:48 +0000

    ...

--- basics/MultidimensionalArrays.java
+++ basics/MultidimensionalArrays.java
@@ -6,11 +6,16 @@ public class MultidimensionalArrays {
         multi_D();
     }
 
+    private static int[][] arr = { { 1, 2 }, { 3, 4 } };
+
     public static int[][] multiD = new int[3][5];
 
     static void multiDimensional() {
+        multiD[0][0] = 12;
+        multiD[0][1] = 5;
+        multiD[0][2] = 9;
 
-        for (int i = 1; i < multiD.length; i++) {
+        for (int i = 0; i < multiD.length; i++) {
             System.out.println(Arrays.toString(multiD[i]));
         }
         System.out.println(multiD.getClass().getSimpleName());
@@ -29,9 +34,12 @@ public class MultidimensionalArrays {
         System.out.println(s.getClass().getSimpleName());
         System.out.println(((Object) k).getClass().getSimpleName());
 
+        // check if a primitive type is an Integer
         if (((Object) k).getClass().getSimpleName().equals("Integer")) {
             System.out.println("Yesss!!");
         }
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
 
     }
 
