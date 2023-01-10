2a253986febb9ed409e83190b6fa5b3b6ac624b5
Ewooral <boahen.cos@gmail.com>
2023-01-10 23:40:55 +0000

    Find the longest substring in an array of strings in java

---
+++ Problem_Solving/Str_LongestSubstring.java
@@ -0,0 +1,37 @@
+public class Str_LongestSubstring {
+
+    public static void aab(String[] aa) {
+        // String[] aa = { "abc", "def" };
+        int i = 0;
+        int j = i + 1;
+        String subString = "";
+        int k = 0;
+        String curStr = aa[i];
+        while (i < aa[i].length()) {
+        
+            while(j < arr)
+            // String nextStr = aa[j];
+            // char cur = curStr.charAt(k);
+            // char nex = nextStr.charAt(k);
+            // if (cur == nex) {
+            //     j++;
+            // } else {
+            //     k++;
+            // }
+
+            // while (k < curStr.length()) {
+            //     subString += cur;
+
+            // }
+            // i++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String[] aa = { "abc", "def" };
+        String[] ab = { "flower", "flow", "flight" };
+        aab(ab);
+        aab(aa);
+    }
+}
