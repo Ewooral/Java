f8402217224bd1091dfca2d1be3a8cf9690d697f
Ewooral <boahen.cos@gmail.com>
2023-01-13 23:14:47 +0000

    chore: substring of all concatenation in java

---
+++ Advanced_Java_Techniques.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+public class Advanced_Java_Techniques {
+
+    public static void main(String[] args) {
+        IntStream.range(0, 5).forEach(System.out::println);
+        // Advanced looping
+    }
+}
