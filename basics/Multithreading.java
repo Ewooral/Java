b48bb20e46abedc132c4d47ef8eadb32e759af4e
Ewooral <boahen.cos@gmail.com>
2023-01-20 23:00:14 +0000

    chore: ...

--- basics/Multithreading.java
+++ basics/Multithreading.java
@@ -3,19 +3,25 @@ public class Multithreading {
     public static void main(String[] args) {
 
         // five threads at a time
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 1; i++) {
             MultithreadThing newThread = new MultithreadThing(i);
             newThread.start();
         }
 
         AnotherWayofUsingMultithreading anotherWayOfThreading = new AnotherWayofUsingMultithreading();
         Thread myThread = new Thread(anotherWayOfThreading);
+        anotherWayOfThreading.eat();
         myThread.start();
 
     }
 }
 
+// Another way of doing multi threading without extending thread
 class AnotherWayofUsingMultithreading implements Runnable {
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
 
     @Override
     public void run() {
