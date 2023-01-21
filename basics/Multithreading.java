0e5b754e5192480a5b2d7213323bf253a04e3537
Ewooral <boahen.cos@gmail.com>
2023-01-21 07:36:38 +0000

    feat: Perform multithreading using the Runnable interface in Java

--- basics/Multithreading.java
+++ basics/Multithreading.java
@@ -16,7 +16,7 @@ public class Multithreading {
     }
 }
 
-// Another way of doing multi threading without extending thread
+// Multi threading using the Runnable interface
 class AnotherWayofUsingMultithreading implements Runnable {
 
     public void eat() {
@@ -26,6 +26,34 @@ class AnotherWayofUsingMultithreading implements Runnable {
     @Override
     public void run() {
         System.out.println("God is Good!");
+    }
+
+}
+
+// Multithreading using Thread class
+class MultithreadThing extends Thread {
+
+    private int thread;
+
+    public MultithreadThing(int num) {
+        this.thread = num;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + " from thread " + this.thread);
+            if (this.thread == 3) {
+                throw new RuntimeException();
+            }
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
