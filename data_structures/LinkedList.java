69dadcf02b8ab86af3be68bfc7a3f9160a6cedf8
Ewooral <boahen.cos@gmail.com>
2023-01-28 23:41:35 +0000

    ...

--- data_structures/LinkedList.java
+++ data_structures/LinkedList.java
@@ -89,37 +89,40 @@ public class LinkedList<T> {
         // Search for the key to be deleted,
         // keep track of the previous node
         // as it is needed to change currNode.next
-        while (currNode != null && currNode.data != key) {
+        // while (currNode != null && currNode.data != key) {
             // If currNode does not hold key
             // continue to next node
             prev = currNode;
             currNode = currNode.next;
         }
 
-        // If the key was present, it should be at currNode
-        // Therefore the currNode shall not be null
-        if (currNode != null) {
-            // Since the key is at currNode
-            // Unlink currNode from linked list
-            prev.next = currNode.next;
+    // If the key was present, it should be at currNode
+    // Therefore the currNode shall not be null
+    if(currNode!=null)
 
-            // Display the message
-            System.out.println(key + " found and deleted");
-        }
+    {
+        // Since the key is at currNode
+        // Unlink currNode from linked list
+        prev.next = currNode.next;
 
-        //
-        // CASE 3: The key is not present
-        //
+        // Display the message
+        System.out.println(key + " found and deleted");
+    }
 
-        // If key was not present in linked list
-        // currNode should be null
-        if (currNode == null) {
-            // Display the message
-            System.out.println(key + " not found");
-        }
+    //
+    // CASE 3: The key is not present
+    //
 
-        // return the List
-        return list;
+    // If key was not present in linked list
+    // currNode should be null
+    if(currNode==null)
+    {
+        // Display the message
+        System.out.println(key + " not found");
+    }
+
+    // return the List
+    return list;
     }
 
     // **************DELETION AT A POSITION**************
