06eec014ce06b15dbff45e55416fdb7e1a795ef3
Ewooral <boahen.cos@gmail.com>
2023-02-22 23:47:35 +0000

    chore: rename a java file

--- Problem_Solving/Arr_Twosum.java
+++ Problem_Solving/Arr_Twosum.java
@@ -1,3 +1,5 @@
+package Problem_Solving;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,17 +9,17 @@ public class Arr_Twosum {
     // find two numbers when added sum up to target.
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(2, 4, -3, 1, 3, 0, -3, -2));
-        twoSum(arr, 1);
+        twoSum(arr);
 
     }
 
-    static void twoSum(ArrayList<Integer> arr, int target) {
+    static void twoSum(ArrayList<Integer> arr) {
         for (int i = 0; i < arr.size(); i++) {
             int curNum = arr.get(i);
 
             for (int j = 1; j < arr.size(); j++) {
                 int nextNum = arr.get(j);
-                if (curNum + nextNum == target && i != j) {
+                if (curNum + nextNum == 1 && i != j) {
                     System.out.println(new ArrayList<>(Arrays.asList(curNum, nextNum)));
 
                 }
