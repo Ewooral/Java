069682564ddc22e5ab14f4d63c5276913f2048c2
Ewooral <boahen.cos@gmail.com>
2023-02-28 22:14:38 +0000

    feat: Add a new java class

--- learn-spring-framework/src/main/java/com/codersabode/learnspringframework/SpringBeanVsJavaBean.java
+++ learn-spring-framework/src/main/java/com/codersabode/learnspringframework/SpringBeanVsJavaBean.java
@@ -3,10 +3,11 @@ package com.codersabode.learnspringframework;
 public class SpringBeanVsJavaBean {
     public static void main(String[] args) {
         Pojo pojo = new Pojo();
+        System.out.println(pojo);
     }
 }
 
-class Pojo{
+class Pojo{ // Pojo means plain old java object
 private String text;
 private int number;
 
