0f170c597dd6ec64eac05c148455c9ca39a6831e
Ewooral <boahen.cos@gmail.com>
2023-03-06 23:54:45 +0000

    fix: Correct bean name

--- learn-spring-framework/src/main/java/com/codersabode/learnspringframework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/codersabode/learnspringframework/App02HelloWorldSpring.java
@@ -19,7 +19,7 @@ public class App02HelloWorldSpring {
 //        System.out.println(context.getBean("age"));
 //        System.out.println(context.getBean("person"));
 //        System.out.println(context.getBean("person2"));
-        System.out.println(context.getBean("person3Parameters"));
+//        System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("addressII"));
         System.out.println("School ID: " + context.getBean("school_id"));
          //  System.out.println(context.getBean(Address.class));
