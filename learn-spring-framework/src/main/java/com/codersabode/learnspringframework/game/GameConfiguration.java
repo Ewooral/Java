59c8db92c1ab44a9b29de55ff2d36f7ec09a16cf
Ewooral <boahen.cos@gmail.com>
2023-03-21 21:51:49 +0000

    chore: Refactor code by moving files into their respective packages

--- learn-spring-framework/src/main/java/com/codersabode/learnspringframework/game/GameConfiguration.java
+++ learn-spring-framework/src/main/java/com/codersabode/learnspringframework/game/GameConfiguration.java
@@ -7,27 +7,14 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class GameConfiguration {
     @Bean
-    @Primary
     public GamingConsole game() {
         return new PackManGame();
-    }
-
-    @Bean
-    public GamingConsole newGame() {
-        return new MarioGame();
     }
 
     @Bean
     public GameRunner gameRunner(GamingConsole game) {
         // object creation + wiring of dependencies
         return new GameRunner(game);
-    }
-
-    @Bean
-    @Primary
-    public GameRunner gameRunner1(GamingConsole newGame) {
-        // object creation + wiring of dependencies
-        return new GameRunner(newGame);
     }
 
 }
