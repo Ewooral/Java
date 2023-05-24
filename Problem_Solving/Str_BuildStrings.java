3d0e50d31b0d792281b8569e8f7076f4c94d6fb6
Ewooral <boahen.cos@gmail.com>
2023-05-24 23:57:03 +0000

    build strings with java

--- Problem_Solving/Str_BuildStrings.java
+++ Problem_Solving/Str_BuildStrings.java
@@ -61,7 +61,7 @@ public class Str_BuildStrings {
     public static int countGoodStrings(int low, int high, int zero, int one) {
         int[] dp = new int[high + 1];
         dp[0] = 1;
-        // int md = 1000000007;
+        int md = 1000000007;
         for (int i = Math.min(zero, one); i <= high; i++) {
             if (i >= zero) {
                 dp[i] = (dp[i] + dp[i - zero]) % md;
