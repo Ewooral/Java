8a20bd57f39bf4db918a65574e38139d154e480c
Ewooral <boahen.cos@gmail.com>
2023-05-25 00:00:58 +0000

    build strings with java

--- Problem_Solving/Str_BuildStrings.java
+++ Problem_Solving/Str_BuildStrings.java
@@ -54,7 +54,7 @@ public class Str_BuildStrings {
 
     public static void main(String[] args) {
 
-        System.out.println("Build Strings");
+        System.out.println(countGoodStrings(2, 3, 1, 2));
 
     }
 
